d330f88bea03512016fd7519d61a39173248209f
jou <116296786+jouaraujo@users.noreply.github.com>
2025-04-25 10:52:04 -0300

    Add files via upload

---
+++ aula05/consulta2.sql
@@ -0,0 +1,13 @@
+SELECT 
+	st_distance(st_transform(c.geom, 31982), st_transform(v.geom, 31982)) AS distancia
+FROM
+	caminhos as c, vegetacao as v
+WHERE c.cod = 1 AND v.cod = 1;
+
+--------------------------------------------------------------------------------------------------
+
+SELECT
+	st_area(st_transform(geom, 31982)) AS metros
+FROM
+	restaurante 
+WHERE cod = 1;
