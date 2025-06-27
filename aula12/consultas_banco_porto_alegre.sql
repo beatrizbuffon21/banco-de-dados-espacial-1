ea99f5edf5a3058f2eaa157aa0c2439b77583a46
jou <116296786+jouaraujo@users.noreply.github.com>
2025-06-27 10:33:21 -0300

    Add files via upload

---
+++ aula12/consultas_banco_porto_alegre.sql
@@ -0,0 +1,28 @@
+-- Criando a extensão
+CREATE EXTENSION postgis;
+
+-- Visualizando school
+SELECT *
+FROM school;
+
+-- Visualizando supermarket
+SELECT *
+FROM supermarket;
+
+-- Visualizando transporte_publico
+SELECT *
+FROM transporte_publico
+
+-- Visualizando admin_9
+SELECT *
+FROM admin_9;
+
+-- Visualizando admin_10
+SELECT *
+FROM admin_10;
+
+-- Visualizando porto_alegre
+SELECT *
+FROM porto_alegre;
+
+
---
+++ aula12/criando_tabela.sql
@@ -0,0 +1,7 @@
+-- Criando a tabela porto_alegre
+CREATE TABLE porto_alegre as
+SELECT *
+FROM rs
+WHERE nome = 'Porto Alegre';
+
+
